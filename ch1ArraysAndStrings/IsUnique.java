ec7da88b1d27916239fae1820a057c19e53a2b9b
Adrien Tremblay <adrien@adrientremblay.com>
2022-11-13 11:17:06 -0500

    bit vector solution

--- ch1ArraysAndStrings/IsUnique.java
+++ ch1ArraysAndStrings/IsUnique.java
@@ -1,3 +1,5 @@
+package ch1ArraysAndStrings;
+
 public class IsUnique {
     public boolean arraySolution(String s) {
         boolean[] history = new boolean[256];
@@ -12,12 +14,31 @@ public class IsUnique {
         return true;
     }
 
+    /**
+     * Assumes the char set is only lowercase alpha chars so that we can use
+     * a single int as the history
+     * @param s
+     * @return
+     */
+    public boolean bitVectorSolution(String s) {
+        int history = 0;
+
+        for (char c : s.toCharArray()) {
+            int val = c - 'a';
+            if ((history & (1 << val)) > 0)
+                return false;
+            history |= (1 << val);
+        }
+
+        return true;
+    }
+
     public static void main(String args[]) {
         IsUnique isUnique = new IsUnique();
         String[] inputs = {"asdf", "asdfa", "", "b"};
 
         for (String s : inputs) {
-            System.out.println(isUnique.arraySolution(s));
+            System.out.println(isUnique.arraySolution(s) + " " + isUnique.bitVectorSolution(s));
         }
     }
 }
