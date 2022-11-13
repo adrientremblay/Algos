17ff5d76100f4827c85f4cef0633073ece63c1d9
Adrien Tremblay <adrien@adrientremblay.com>
2022-11-13 11:43:10 -0500

    book solution for check permutation

--- ch1ArraysAndStrings/CheckPermutation.java
+++ ch1ArraysAndStrings/CheckPermutation.java
@@ -20,9 +20,40 @@ public class CheckPermutation {
         return true;
     }
 
+    private String sort(String s) {
+        char[] content = s.toCharArray();
+        java.util.Arrays.sort(content);
+        return new String(content);
+    }
+
+    public boolean sortSolution(String s1, String s2) {
+        return sort(s1).equals(sort(s2));
+    }
+
+    public boolean bookSolution(String s1, String s2) {
+        if (s1.length() != s2.length())
+            return false;
+
+        int[] letters = new int[256];
+
+        for (char c : s1.toCharArray())
+            letters[c]++;
+
+        for (char c : s2.toCharArray())
+            if (--letters[c] < 0)
+                return false;
+
+        return true;
+    }
+
     public static void main(String args[]) {
         CheckPermutation checkPermutation = new CheckPermutation();
         System.out.println(checkPermutation.arraySolution("abc", "cba"));
         System.out.println(checkPermutation.arraySolution("dbc", "cba"));
+
+        System.out.println(checkPermutation.sortSolution("abc", "cba"));
+        System.out.println(checkPermutation.sortSolution("dbc", "cba"));
+
+        System.out.println(checkPermutation.bookSolution("aac", "cba"));
     }
 }
