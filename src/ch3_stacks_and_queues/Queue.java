bcfc1cb0c308877cf5c13a6a95d5d93b6bfbc12a
Adrien Tremblay <adrien@adrientremblay.com>
2022-12-15 10:44:18 -0500

    My Queue class

---
+++ src/ch3_stacks_and_queues/Queue.java
@@ -0,0 +1,45 @@
+package ch3_stacks_and_queues;
+
+public class Queue<T> {
+	private class QueueNode {
+		public T value;
+		public QueueNode next;
+		
+		public QueueNode(T value) {
+			this.value = value;
+			next = null;
+		}
+	}
+	
+	private QueueNode head;
+	private QueueNode tail;
+	
+	public void add(T value) {
+		QueueNode newNode = new QueueNode(value);
+		if (tail == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = tail.next;
+		}
+	}
+
+	public T remove() {
+		if (head == null)
+			return null;
+		T ret = head.value;
+		head = head.next;
+		return ret;
+	}
+
+	public T peek() {
+		if (head == null)
+			return null;
+		return head.value;
+	}
+
+	public boolean isEmpty() {
+		return head == null;
+	}
+}
