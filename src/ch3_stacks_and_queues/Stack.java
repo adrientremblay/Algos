db87d3738890799c0468daba876b779ad30daf72
Adrien Tremblay <adrien@adrientremblay.com>
2022-12-15 10:23:27 -0500

    Write my implementation of the Stack

---
+++ src/ch3_stacks_and_queues/Stack.java
@@ -0,0 +1,40 @@
+package ch3_stacks_and_queues;
+
+public class Stack<T> {
+	private class StackNode {
+		public T value;
+		public StackNode next;
+		
+		public StackNode(T value) {
+			this.value = value;
+			next = null;
+		}
+	}
+	
+	private StackNode head = null;
+
+	public void push(T value) {
+		StackNode newNode = new StackNode(value);
+		if (head != null)
+			newNode.next = head;
+		head = newNode;
+	}
+	
+	public T pop() {
+		if (head == null)
+			return null;
+
+		T ret = head.value;
+		head = head.next;
+		return ret;
+	}
+	
+	public T peek() {
+		return head.value;
+	}
+	
+	public boolean isEmpty() {
+		return head == null;
+	}
+
+}
