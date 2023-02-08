8ebb8136fa37c24f096af977a073ec51a95ab2cd
Adrien Tremblay <adrien@adrientremblay.com>
2023-02-08 13:00:35 -0500

    Working method to save and print parenthesis locatins

--- src/clrs/ch14_dynamic_programming/MatrixChainMultiplication.java
+++ src/clrs/ch14_dynamic_programming/MatrixChainMultiplication.java
@@ -16,6 +16,45 @@ public class MatrixChainMultiplication {
 		}
 		return best;
 	}
+
+	public static int naiveTopDownSolutionExtended(int[][] matrices, int i, int j, int[][] parenthesisLocations) {
+		if (i == j)
+			return 0;
+		
+		if (i + 1 == j)
+			return matrices[i][0] * matrices[i][1] * matrices[j][1];
+		
+		int best = Integer.MAX_VALUE;
+		for (int k = i ; k <= j-1 ; k++) {
+			int cost = naiveTopDownSolution(matrices, i, k) 
+					+ naiveTopDownSolution(matrices, k+1, j) 
+					+ (matrices[i][0] * matrices[k][1] * matrices[j][1]);
+			if (cost < best) {
+				best = cost;
+				parenthesisLocations[i][j] = k;
+			}
+		}
+		return best;
+	}
+	
+	public static void printParenthesisLocations(int[][] parenthesisLocations, int i , int j) {
+		if (i == j) {
+			System.out.print("A" + (i+1));
+			return;
+		}
+
+		if (i + 1 == j) {
+			System.out.print("(A" + (i+1));
+			System.out.print("A" + (j+1) + ")");
+			return;
+		}
+		
+		System.out.print('(');
+		int k = parenthesisLocations[i][j];
+		printParenthesisLocations(parenthesisLocations,i, k);
+		printParenthesisLocations(parenthesisLocations,k+1, j);
+		System.out.print(')');
+	}
 	
 	
 	public static void main(String args[]) {
@@ -27,8 +66,12 @@ public class MatrixChainMultiplication {
 			{20, 10}
 		};
 		
-		int ans = naiveTopDownSolution(sampleMatrices, 0, 4);
+		int[][] parenthesisLocations = new int[5][5];
+		
+		int ans = naiveTopDownSolutionExtended(sampleMatrices, 0, 4, parenthesisLocations);
 		System.out.println(ans);
+		
+		printParenthesisLocations(parenthesisLocations, 0, 4);
 	}
 
 }
