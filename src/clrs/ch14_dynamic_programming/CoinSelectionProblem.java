d9bfe6b5fc580f692f97b81ce874a5f24a21c20c
Adrien Tremblay <adrien@adrientremblay.com>
2023-02-10 16:25:22 -0500

    Coin selection problem, row of 1's not required

--- src/clrs/ch14_dynamic_programming/CoinSelectionProblem.java
+++ src/clrs/ch14_dynamic_programming/CoinSelectionProblem.java
@@ -6,17 +6,21 @@ import src.util.Util;
  * Find the minimal amount of coins of different specified values to reach a specified total value
  */
 public class CoinSelectionProblem {
-    public static int dpSolution(int values[], int total) {
+    public static int dpSolution(int values[], int total) throws Exception {
         int[][] m = new int[values.length][total + 1];
 
         for (int i = 0 ; i < m.length ; i++) {
             for (int j = 1 ; j < m[i].length ; j++) {
                 if (i == 0) {
                     m[i][j] = (j % values[i] == 0) ? (j / values[i]) : (-1);
-                } else if (j < values[i]){
-                    m[i][j] = m[i-1][j];
-                } else if (m[i-1][j] == -1) {
+                } else if (j < values[i]) {
+                    m[i][j] = m[i - 1][j];
+                } else if (m[i-1][j] == -1 && m[i][j-values[i]] == -1) {
+                    throw new Exception("bruh");
+                } else if (m[i-1][j] == -1 ) {
                     m[i][j] = m[i][j-values[i]] + 1;
+                } else if (m[i][j-values[i]] == -1) {
+                    m[i][j] = m[i - 1][j];
                 } else {
                     m[i][j] = Math.min(m[i-1][j], m[i][j-values[i]] + 1);
                 }
@@ -48,15 +52,15 @@ public class CoinSelectionProblem {
         return m[values.length - 1][total];
     }
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws Exception {
         int[] values = {1, 5, 6, 8};
         int target = 11;
 
-        dpSolutionMustHave1(values, target);
+        dpSolution(values, target);
 
         int[] values2 = {2, 5, 6, 8};
         int target2 = 11;
 
-        dpSolutionMustHave1(values2, target2);
+        dpSolution(values2, target2);
     }
 }
